1d93f27479422ddcf818d26d4480c5164aa0f0c7
cindy-ta <cindy-ta@uiowa.edu>
2016-10-07 16:30:07 -0500

    Update movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -46,29 +46,85 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
+    # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
     # Each returned movie will be a hash representing one row of the movies_table
     # The keys will be the table headers and the values will be the row contents.
     # Entries can be directly to the database with ActiveRecord methods
     # Add the necessary Active Record call(s) to populate the database.
+    if (! Movie.find_by_title(movie[:title]))
+        Movie.create!(movie)
+    end
   end
 end
 
 When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
-  # HINT: use String#split to split up the rating_list, then
-  # iterate over the ratings and check/uncheck the ratings
-  # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
+  ratings = "G PG PG-13 NC-17 R".split(" ")
+  
+  ratings.each do |rating|
+    uncheck "ratings_#{rating}"
+  end
+  
+  arg1.split(', ').each do |rating|
+    check "ratings_#{rating}"
+  end
+  
+  click_button "ratings_submit"
+end
+
+When (/^I have opted to alphabetically sort movies$/) do
+    click_on "title_header"
+end
+
+When (/^I have opted to sort movies in most recent order of release date$/) do
+    click_on "release_date_header"
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+  ratings = "G PG PG-13 NC-17 R".split(" ")
+  
+  arg1.split(', ').each do |rating|
+    page.has_checked_field? "ratings_#{rating}"
+    ratings.delete(rating)
+  end
+  
+  ratings.each do |rating|
+    page.has_unchecked_field? "ratings_#{rating}"
+  end
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+  size = Movie.all.size
+  size.should == page.all('table#movies tr').count-1
 end
 
+Then (/^I should see movie title "(.*?)" before "(.*?)"$/) do |title1, title2|
+    
+    titleBool = false
+    list = []
+    
+    all("table#movies tbody/tr/td[1]").each do |title|
+        list.push(title.text)
+    end
+    
+    if (list.index(title2) > list.index(title1))
+        titleBool = true
+    end
+    
+    expect(titleBool).to be_truthy
+end
 
-
+Then (/^I should see the date "(.*?)" before "(.*?)"$/) do |date1,date2|
+    dateBool = false
+    list = []
+    
+    all("table#movies tbody/tr/td[3]").each do |date|
+        list.push(date.text)
+    end
+    
+    if (list.index(date2) > list.index(date1))
+        dateBool = true
+    end
+    
+    expect(dateBool).to be_truthy
+end
